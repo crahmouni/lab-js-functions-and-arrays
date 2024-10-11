c276db5b2484e6b1f9cb66fc52feceb1fa5ca8de
Chahinaz <crahmouni@uoc.edu>
2024-10-11 23:33:29 +0200

    WD-PT-SEPT34-ES-CHAHINAZ-lab-js-functions-and-arrays

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+    let max = a;
+    for (let i = 0; i < 1; i++) {
+        if (b > max) {
+            max = b;
+        }
+    }
+    return max;
+}
 
 
 
@@ -7,7 +15,18 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null;
+    }
+    let longestWord = words[0];
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+   return longestWord; 
+}
 
 
 
@@ -15,7 +34,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
 
 
 
@@ -23,7 +48,23 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+    function sumNumbers(numbers) {
+        let sum = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    const sum = sumNumbers(numbers);
+    const average = sum / numbers.length;
+    
+    return average;
+
+}
 
 
 
@@ -31,4 +72,17 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, wordToSearch) {
+    if (words.length === 0) {
+        return null;  
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] === wordToSearch) {
+            return true; 
+        }
+    }
+
+    return false;
+}
+
